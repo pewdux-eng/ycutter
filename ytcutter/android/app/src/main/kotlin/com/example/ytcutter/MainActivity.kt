d4055036a41a440cc7205a8ddd00178465a44aaf
pewdux-eng <pewdux@gmail.com>
2026-03-22 22:09:12 -0400

    Create MainActivity.kt

---
+++ ytcutter/android/app/src/main/kotlin/com/example/ytcutter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ytcutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
+```
+
+**File 2:** `ytcutter/android/gradle.properties`
+```
+org.gradle.jvmargs=-Xmx1536M
+android.useAndroidX=true
+android.enableJetifier=true
